f0be56dc5cb9e5c2ae09a2d3ebc5cf1123fbb470
Gabriel Thibeault <gabriel.eric.thibeault@gmail.com>
2021-12-10 22:35:56 +0100

    Added Constants class

---
+++ ToddProblem/ToddProblem/Constants.py
@@ -0,0 +1,11 @@
+from typing import ClassVar
+
+
+class Constants(object):
+
+    epsilon = 0.0005
+    preprocessor = "preprocessor"
+    weighter = "weighter"
+    matcher = "matcher"
+    markov = "markov"
+    priority = "priority"
