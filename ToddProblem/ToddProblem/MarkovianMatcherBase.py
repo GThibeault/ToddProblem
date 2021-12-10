5e91a5304f8113f9fe2d669453a6d05a094ee8c4
Gabriel Thibeault <gabriel.eric.thibeault@gmail.com>
2021-12-10 02:21:24 +0100

    Created base matcher class

---
+++ ToddProblem/ToddProblem/MarkovianMatcherBase.py
@@ -0,0 +1,24 @@
+class MarkovianMatcherBase(object):
+
+    def match(self, rankingMatrix, stationaryState):
+        rankedStationary = sorted(
+            enumerate(stationaryState), key=lambda s: s[1], reverse=True)
+
+        pairs = []
+        taken = {i: False for i in range(len(rankingMatrix))}
+
+        for index, weight in rankedStationary:
+            if taken[index]:
+                continue
+
+            rankedPreference = sorted(
+                enumerate(rankingMatrix[index]), key=lambda s: s[1] if s[0] != index else len(rankingMatrix), reverse=False)
+
+            matched = self.match_pair(rankedPreference, taken)
+
+            taken[index] = True
+            taken[matched] = True
+
+            pairs.append((index, matched))
+
+        return pairs
--- ToddProblem/ToddProblem/MarkovianPriorityMatcher.py
+++ ToddProblem/ToddProblem/MarkovianPriorityMatcher.py
@@ -1,33 +1,9 @@
-class MarkovianPriorityMatcher(object):
+from MarkovianMatcherBase import MarkovianMatcherBase
 
-    def match(self, rankingMatrix, stationaryState):
-        rankedStationary = sorted(
-            enumerate(stationaryState), key=lambda s: s[1], reverse=True)
 
-        pairs = []
-        taken = {i: False for i in self.__getRankingRange(rankingMatrix)}
+class MarkovianPriorityMatcher(MarkovianMatcherBase):
+    def match_pair(self, rankedPreference, taken):
+        nextAvailable = next(
+            filter(lambda p: not taken[p[0]], rankedPreference))
 
-        for index, weight in rankedStationary:
-            if taken[index]:
-                continue
-
-            rankedPreference = sorted(
-                enumerate(rankingMatrix[index]), key=lambda s: s[1] if s[0] != index else len(rankingMatrix), reverse=False)
-
-            firstAvailable = next(
-                filter(lambda p: self.__isAvailable(p, taken), rankedPreference))[0]
-
-            taken[index] = True
-            taken[firstAvailable] = True
-
-            pairs.append((index, firstAvailable))
-
-        return pairs
-
-    def __getRankingRange(self, rankingMatrix):
-        return range(len(rankingMatrix))
-
-    def __isAvailable(self, preference, taken):
-        prefIndex, prefRanking = preference
-
-        return not taken[prefIndex]
+        return nextAvailable[0]
