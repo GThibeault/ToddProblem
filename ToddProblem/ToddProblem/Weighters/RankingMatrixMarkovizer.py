eadbf30b5e55a6e8e4a169932d6862e59181682d
Gabriel Thibeault <gabriel.eric.thibeault@gmail.com>
2021-12-13 23:07:02 +0100

    Improved markovizer

--- ToddProblem/ToddProblem/Weighters/RankingMatrixMarkovizer.py
+++ ToddProblem/ToddProblem/Weighters/RankingMatrixMarkovizer.py
@@ -4,13 +4,11 @@ from numpy import transpose
 class RankingMatrixMarkovizer(object):
 
     def execute(self, rankingMatrix):
-        groupSize = len(rankingMatrix)
-        rankingSum = sum(range(groupSize))
-
         columns = []
 
         for rankingList in rankingMatrix:
-            markovColumn = [x / rankingSum for x in rankingList]
+            markovColumn = [x / sum(rankingList)
+                            for x in rankingList]
             columns.append(markovColumn)
 
         return transpose(columns)
